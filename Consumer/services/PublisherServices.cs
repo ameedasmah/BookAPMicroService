dc4b538d0860405d9b627eef436b39356a925eef
ameedasmah <ameed.asmah@hotmail.com>
2021-06-20 10:59:58 +0300

    done with service create

---
+++ Consumer/services/PublisherServices.cs
@@ -0,0 +1,60 @@
+﻿using Contract.Entities;
+using Contract.Resourse;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Consumer.services
+{
+    public interface IPublisher
+    {
+        Task CreatePublisher(int Id);
+        Task UpdatePublisher(int Id);
+        Task RemovePublisher(int Id);
+    }
+    public interface ToRecive
+    {
+        public int Id { get; set; }
+        public Publisher publisher { get; set; }
+        public string Type { get; set; }
+    }
+    class PublisherServices : IPublisher
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IPublisher _publisher;
+
+        public PublisherServices(HttpClient httpClient,IPublisher publisher)
+        {
+            _httpClient = httpClient;
+            _publisher = publisher;
+        }
+
+        string URI = "http://localhost/44325/api/";
+
+        public async Task CreatePublisher(int Id)
+        {
+            Uri geturi = new Uri(URI + "Publisher/" + Id); 
+
+            var response = await _httpClient.GetAsync(geturi);
+            response.EnsureSuccessStatusCode();
+             var responseString = await response.Content.ReadAsStringAsync();
+            var data =  JsonConvert.DeserializeObject<PublisherResource>(responseString);
+            Console.WriteLine($"myData{data}");
+        }
+
+        public Task RemovePublisher(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdatePublisher(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/mangers/PublisherManger.cs
@@ -0,0 +1,103 @@
+﻿using Contract.Entities;
+using Contract.models;
+using Contract.Resourse;
+using Domain.mangers.Producer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Helper;
+using WebApplication1.Repositories;
+
+
+namespace Domain.mangers
+{
+    public interface IPublisherManger
+    {
+        Task<IEnumerable<PublisherResource>> GetPublishers();
+        Task<Publisher> GetPublisher(int id);
+        Task<PublisherResource> CreatePublisher(PublisherModel newPublisherModel);
+        Task<PublisherResource> PutPublisher(int id, PublisherModel model);
+        Task DeleteResource(int Id);
+    }
+    public class publishermanger : IPublisherManger
+    {
+
+        private readonly IPublisherRepositories _repository;
+        private readonly IPublisherSend _publisherSend;
+        public publishermanger(IPublisherRepositories repository, IPublisherSend publisherSend)
+        {
+            _repository = repository;
+            _publisherSend = publisherSend;
+        }
+        public async Task<PublisherResource> CreatePublisher(PublisherModel newPublisherModel)
+        {
+            var newPublisherEntity = new Publisher()
+            {
+                Name = newPublisherModel.Name,
+                Email = newPublisherModel.Email,
+                DateOfBirth = newPublisherModel.DateOfBirth,
+                Salery = newPublisherModel.Salery
+            };
+            var newPublisherResource = await _repository.CreatePublisher(newPublisherEntity);
+            _publisherSend.sendPublisher(new SendArgument()
+            {
+                Id = newPublisherEntity.Id,
+                Type = "Create"
+            });
+            return newPublisherResource.ToResource();
+
+
+        }
+
+        public async Task DeleteResource(int Id)
+        {
+            var BookToDelete = await _repository.GetPublisher(Id);
+            if (BookToDelete == null) throw new Exception("Id not Found");
+            _publisherSend.sendPublisher(new SendArgument()
+            {
+                Id = BookToDelete.Id,
+                Type = "Delete"
+            });
+
+            await _repository.deletePublisher(BookToDelete.Id);
+        }
+
+        public async Task<Publisher> GetPublisher(int id)
+        {
+            return await _repository.GetPublisher(id);
+        }
+
+        public async Task<IEnumerable<PublisherResource>> GetPublishers()
+        {
+            var PublisherEntities = await _repository.GetPublishers();
+
+            var publisherResource = new List<PublisherResource>();
+
+            foreach (var item in PublisherEntities)
+            {
+                publisherResource.Add(item.ToResource());
+            }
+            return publisherResource;
+        }
+
+        public async Task<PublisherResource> PutPublisher(int id, PublisherModel model)
+        {
+            var existingEntity = await _repository.GetPublisher(id);
+            if (existingEntity == null) throw new Exception("Id not Found");
+
+            existingEntity.Name = model.Name;
+            var updatedEntity = await _repository.updatePublisher(existingEntity);
+            _publisherSend.sendPublisher(new SendArgument()
+            {
+                Id = existingEntity.Id,
+                Type = "Update"
+            });
+            return updatedEntity.ToResource();
+        }
+    }
+}
+
