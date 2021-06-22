2b13eafc7b32ccafea9ce5d8f9165135fa7f5832
ameedasmah <ameed.asmah@hotmail.com>
2021-06-22 13:47:30 +0300

    Done with MessageQueue Controller

--- WebApplication1/Controllers/HarvestController.cs
+++ WebApplication1/Controllers/HarvestController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers
@@ -12,6 +14,44 @@ namespace WebApplication1.Controllers
     public class HarvestController : ControllerBase
     {
         [HttpGet]
+        public void GetPublishers()
+        {
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672,
+                };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Harvest",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+                    string message = "Hello World!, Its Work:DDD";
+                    var body = Encoding.UTF8.GetBytes(message);
 
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Harvest",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+            catch (Exception EX)
+            {
+                throw new Exception($"there id an error {EX.Message}");
+            };
+        }
     }
+
+
+
 }
